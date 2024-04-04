0325ac10dd76a556f6288ff7f4e4dbe78465b33d
Spandan Barve <spandan567@gmail.com>
2024-04-04 15:25:03 +0530

    remove unused package

--- src/client.ts
+++
@@ -1,9 +0,0 @@
-import { createWalletClient, custom, publicActions } from "viem";
-import { sepolia } from "viem/chains";
-
-const client = createWalletClient({
-  chain: sepolia,
-  transport: custom((window as any).ethereum),
-}).extend(publicActions);
-
-export default client;
